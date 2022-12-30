66e1b3bf0826e2a176805e0c9a2496d7f2511bd1
rebecabergottini <rebeca._bergo@hotmail.com>
2022-12-30 22:13:37 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var domain = [".com", ".net", ".us", ".io", ".es"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(
+          pronoun[i] +
+            adj[j] +
+            noun[k] +
+            domain[Math.floor(Math.random() * domain.length)]
+        );
+      }
+    }
+  }
 };
