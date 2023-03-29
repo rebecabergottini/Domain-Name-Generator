ce52ae721f022bf249709e371ad0e9adcc78ba58
rebecabergottini <rebeca._bergo@hotmail.com>
2023-03-29 17:48:57 +0000

    my first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io", ".es"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(
+          pronoun[i] +
+            adj[j] +
+            noun[k] +
+            domain[Math.floor(Math.random() * domain.length)]
+        );
+      }
+    }
+  }
 };
